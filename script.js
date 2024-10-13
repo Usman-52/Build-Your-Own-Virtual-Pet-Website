ba067ccddccea7933678569c8d031f773f3db401
Usman-52 <usman914.764@gmail.com>
2024-10-13 12:49:57 +0500

    add feed function

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var yourPet = {
     age: 0,
     happiness: 0,
     hunger: 0,
+    feed: function() {
+        if (this.hunger > 100) this.hunger = 100;
+        this.hunger -= 20;
+        if (this.hunger < 0) this.hunger = 0;
+        alert(`${this.name} is fed, and the hunger level is ${this.hunger}`);
+    },
+
     
 }
 
